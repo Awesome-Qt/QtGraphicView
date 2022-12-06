c50cf40471bbfbec3fb985bf6a515c9abbb3c58c
liangtie.qian <liangtie.qian@cyclone-robotics.com>
2022-12-06 14:16:57 +0800

    Enabel preset styles

--- include/QtGraphicView/FlowStyleCollection.h
+++ include/QtGraphicView/FlowStyleCollection.h
@@ -5,24 +5,49 @@
 
 #include <QColor>
 #include <QPixmap>
+#include <memory>
 
 #include "QtGraphicView/QtGraphicView_export.hpp"
 
 struct FlowViewStyle;
 struct NodeStyle;
 
+enum PresetStyle
+{
+    LIGHT,
+    DARK,
+};
+
 class QTGRAPHICVIEW_EXPORT FLowStyleCollection
 {
-public:
-  virtual ~FLowStyleCollection() = default;
+  public:
+    FLowStyleCollection(const FLowStyleCollection&) = default;
+    FLowStyleCollection(FLowStyleCollection&&) = delete;
+    FLowStyleCollection& operator=(const FLowStyleCollection&) = default;
+    FLowStyleCollection& operator=(FLowStyleCollection&&) = delete;
+    virtual ~FLowStyleCollection() = default;
 
-  [[nodiscard]] virtual const FlowViewStyle& flowViewStyle() const;
+    [[nodiscard]] const FlowViewStyle& flowViewStyle() const;
 
-  [[nodiscard]] virtual const NodeStyle& nodeStyle() const;
+    [[nodiscard]] const NodeStyle& nodeStyle() const;
 
-  static FLowStyleCollection& defaultStyle();
+    [[nodiscard]] const FlowViewStyle& flowViewStyle(PresetStyle preset) const;
 
-private:
+    [[nodiscard]] const NodeStyle& nodeStyle(PresetStyle preset) const;
+
+    void setDefaultStyle(PresetStyle style);
+
+    void setNodeStyle(const NodeStyle&);
+
+    void setFlowViewStyle(const FlowViewStyle&);
+
+    static FLowStyleCollection& defaultStyle();
+
+  private:
+    FLowStyleCollection() = default;
+    std::shared_ptr<FlowViewStyle> flowViewStyle_ {};
+    std::shared_ptr<NodeStyle> nodeStyle_ {};
+    PresetStyle presetStyle_ {};
 };
 
 #endif
--- source/FlowStyleCollection.cpp
+++ source/FlowStyleCollection.cpp
@@ -1,22 +1,60 @@
+#include <memory>
+
 #include "QtGraphicView/FlowStyle.h"
 
 #include "QtGraphicView/FlowStyleCollection.h"
 
-static const auto DefaultFlowViewStyle = FlowViewStyle();
-static const auto DefaultNodeStyle = NodeStyle();
-
 FLowStyleCollection& FLowStyleCollection::defaultStyle()
 {
-  static FLowStyleCollection instance;
-  return instance;
+    static FLowStyleCollection instance;
+    return instance;
 }
 
 const FlowViewStyle& FLowStyleCollection::flowViewStyle() const
 {
-  return DefaultFlowViewStyle;
+    return flowViewStyle_ ? *flowViewStyle_ : flowViewStyle(presetStyle_)
 }
 
 const NodeStyle& FLowStyleCollection::nodeStyle() const
 {
-  return DefaultNodeStyle;
+    return nodeStyle_ ? *nodeStyle_ : nodeStyle(presetStyle_);
+}
+
+const FlowViewStyle& FLowStyleCollection::flowViewStyle(PresetStyle preset) const
+{
+    switch (preset) {
+        case DARK:
+            static const auto kDefaultDark = FlowViewStyle();
+            return kDefaultDark;
+        case LIGHT:
+            static const auto kDefaultLight = FlowViewStyle();
+            return kDefaultLight;
+    }
+}
+
+const NodeStyle& FLowStyleCollection::nodeStyle(PresetStyle preset) const
+{
+    switch (preset) {
+        case DARK:
+            static const auto kDefaultDark = NodeStyle();
+            return kDefaultDark;
+        case LIGHT:
+            static const auto kDefaultLight = NodeStyle();
+            return kDefaultLight;
+    }
+}
+
+void FLowStyleCollection::setDefaultStyle(PresetStyle style)
+{
+    presetStyle_ = style;
+}
+
+void FLowStyleCollection::setNodeStyle(const NodeStyle& s)
+{
+    nodeStyle_ = std::make_shared<NodeStyle>(s);
+}
+
+void FLowStyleCollection::setFlowViewStyle(const FlowViewStyle& s)
+{
+    flowViewStyle_ = std::make_shared<FlowViewStyle>(s);
 }
